77e6096a40b4042506a505378620b4894b98df95
diegolins10 <dede140126>
2024-06-23 19:03:35 -0300

    Refatorando e criando a injeção de dependencia

---
+++ HrManager.Infrastructure/Persistence/Repositories/EmployeeRepository.cs
@@ -0,0 +1,21 @@
+﻿using HrManager.Core.Entities;
+
+namespace HrManager.Infrastructure.Persistence.Repositories
+{
+    public class EmployeeRepository
+    {
+        private readonly HrManagerDbContext _context;
+
+        public EmployeeRepository(HrManagerDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(Employee employee)
+        {
+            _context.Employees.Add(employee);
+
+            _context.SaveChanges();
+        }
+    }
+}

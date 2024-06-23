39efb272a5e96fa181c0e2a40f34c1a42b082d63
diegolins10 <dede140126>
2024-06-23 20:52:37 -0300

    Criando teste unitario que valida o insert do objeto

--- HrManager.UnitTests/Application/Services/EmployeeServiceTests/EmployeeServiceAddTests.cs
+++ HrManager.UnitTests/Application/Services/EmployeeServiceTests/EmployeeServiceAddTests.cs
@@ -16,10 +16,42 @@ namespace HrManager.UnitTests.Application.Services.EmployeeServiceTests
 		[Fact]
 		public void ValidEmployee_AddIsCalled_ReturnValidEmployeeViewModel()
 		{
-			
+			// Arrange 
+			// cria com dados aleatorio o input model
+			var addEmploymentInputModel = new Fixture().Create<AddEmployeeInputModel>();
+
+			// criando o mock
+			var employeeRepositoryMock = new Mock<IEmployeeRepository>();
+
+			// adicionando o mock ao nosso service
+			var employeeService = new EmployeeService(employeeRepositoryMock.Object);
+
+
+			// Act
+			// adicionando objeto gerado pelo fixture
+			var result = employeeService.Add(addEmploymentInputModel);
+
+			// Assert
+
+			// utilizando a asert do proprio XUnit para validar as propriedades
+			Assert.Equal(addEmploymentInputModel.Role, result.Role);
+			Assert.Equal(addEmploymentInputModel.FullName, result.FullName);
+			Assert.Equal(addEmploymentInputModel.Document, result.Document);
+			Assert.Equal(addEmploymentInputModel.BirthDate, result.BirthDate);
+			Assert.Equal(addEmploymentInputModel.RoleLevel, result.RoleLevel);
+			Assert.Equal(addEmploymentInputModel.Role, result.Role);
+
+			// utilizando a lib shouldly para validar as propriedades
+            result.FullName.ShouldBe(addEmploymentInputModel.FullName);
+            result.Document.ShouldBe(addEmploymentInputModel.Document);
+            result.BirthDate.ShouldBe(addEmploymentInputModel.BirthDate);
+            result.RoleLevel.ShouldBe(addEmploymentInputModel.RoleLevel);
+            result.Role.ShouldBe(addEmploymentInputModel.Role);
+
+            employeeRepositoryMock.Verify(er => er.Add(It.IsAny<Employee>()), Times.Once);
 		}
 
-		[Fact]
+        [Fact]
 		public void InvalidBirthDateForEmployee_AddIsCalled_ThrowAnInvalidBirthDateException()
         {
 			
